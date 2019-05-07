035fa210a18e99006fa73328cf0c92bb633a1449
Brian L. Gorman <blgorman@gmail.com>
2019-05-07 00:46:16 -0500

    Migration for List and List Items

--- src/MGS.BoilerplateDemo.EntityFrameworkCore/Migrations/20190507054126_AddOptionListAndOptionListItemsEntities.Designer.cs
+++ src/MGS.BoilerplateDemo.EntityFrameworkCore/Migrations/20190507054126_AddOptionListAndOptionListItemsEntities.Designer.cs
@@ -4,14 +4,16 @@ using MGS.BoilerplateDemo.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MGS.BoilerplateDemo.Migrations
 {
     [DbContext(typeof(BoilerplateDemoDbContext))]
-    partial class BoilerplateDemoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190507054126_AddOptionListAndOptionListItemsEntities")]
+    partial class AddOptionListAndOptionListItemsEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
